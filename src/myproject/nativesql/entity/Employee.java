e139651c6d27d825383c4d4c96daec7d7ab9eff3
Tofique Ahmed  Khan <tofique@webkorps.com>
2022-04-20 16:18:36 +0530

    Hibernate Native Query

---
+++ src/myproject/nativesql/entity/Employee.java
@@ -0,0 +1,57 @@
+package myproject.nativesql.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "emp15")
+public class Employee {
+
+	@Id
+	@Column
+	private int eno;
+	@Column
+	private String ename;
+	@Column
+	private float esal;
+	@Column
+	private String eaddr;
+
+	public int getEno() {
+		return eno;
+	}
+
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public float getEsal() {
+		return esal;
+	}
+
+	public void setEsal(float esal) {
+		this.esal = esal;
+	}
+
+	public String getEaddr() {
+		return eaddr;
+	}
+
+	public void setEaddr(String eaddr) {
+		this.eaddr = eaddr;
+	}
+
+	
+	
+	
+}
---
+++ src/myproject/nativesql/test/Test.java
@@ -0,0 +1,47 @@
+package myproject.nativesql.test;
+
+import java.util.List;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import myproject.nativesql.entity.Employee;
+
+public class Test {
+
+	public static void main(String[] args) {
+		SessionFactory sessionFactory = null;
+		Session session = null;
+		StandardServiceRegistry registry = null;
+		try {
+			Configuration cfg = new Configuration();
+			cfg.configure("/myproject/nativesql/resources/hibernate.cfg.xml");
+			StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
+			builder = builder.applySettings(cfg.getProperties());
+			registry = builder.build();
+			sessionFactory = cfg.buildSessionFactory(registry);
+			session = sessionFactory.openSession();
+			SQLQuery sqlQuery = session.createSQLQuery("select * from emp15");
+			sqlQuery.addEntity(Employee.class);
+			List<Employee> empsList = sqlQuery.list();
+			System.out.println("ENO\tENAME\tESAL\tEADDR");
+			System.out.println("------------------------------------------");
+			for(Employee emp : empsList) {
+				System.out.print(emp.getEno()+"\t");
+				System.out.print(emp.getEname()+"\t");
+				System.out.print(emp.getEsal()+"\t");
+				System.out.println(emp.getEaddr());
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+			sessionFactory.close();
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+}
